cd7ddec629329d6ea4c9348626138a39d34be8f3
saber2pr <saber2pr@gmail.com>
2019-05-24 21:01:31 +0000

    feat(webpack.config.js): add hash

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@
  * @Author: saber2pr
  * @Date: 2019-05-22 20:20:01
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-05-24 20:56:50
+ * @Last Modified time: 2019-05-24 21:00:23
  */
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CleanCSSPlugin = require('less-plugin-clean-css')
 const path = require('path')
 
-const extractLess = new ExtractTextPlugin('style.css')
+const extractLess = new ExtractTextPlugin('style-[hash].css')
 
 const templateContent = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Github</title></head><body></body></html>'
 
@@ -24,7 +24,7 @@ module.exports = WebpackConfig({
     extensions: ['.js', '.ts', '.tsx']
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle-[hash].js',
     path: path.join(__dirname, 'build')
   },
   module: {

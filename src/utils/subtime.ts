25c17680d8ea5b37bdf8f98a72128dfe6ce5444f
saber2pr <saber2pr@gmail.com>
2019-05-26 21:12:13 +0000

    fix(utils/subtime): correct not -8

--- src/utils/subtime.ts
+++ src/utils/subtime.ts
@@ -2,7 +2,7 @@ export const timeDelta = (time1: string, time2: string) => {
   const Time1 = new Date(time1).getTime()
   const Time2 = new Date(time2).getTime()
 
-  const hours = Math.round((Time1 - Time2) / 1000 / 60 / 60 - 8)
+  const hours = Math.round((Time1 - Time2) / 1000 / 60 / 60)
 
   const days = Math.round(hours / 24)
 

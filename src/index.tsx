31a6b3d6ecdbf72ea4423d6f46abe3e937460160
saber2pr <saber2pr@gmail.com>
2019-05-28 16:51:13 +0000

    refactor(core): update @saber2pr/router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router } from '@saber2pr/router'
+import Router from '@saber2pr/router'
 import {
   News,
   Repo,
@@ -18,8 +18,8 @@ import './index.less'
 const container = document.createElement('div')
 document.body.append(container)
 
-Router.Config.render = ReactDOM.render
-Router.Config.container = container
+Router.container = container
+Router.render = ReactDOM.render
 
 Router({
   '/': '/profile',
